b47e63c9992a9c5c6e3438390c0e24098b129d29
Adrien Da Silva Ferreira <adrien@utopios.solutions>
2024-09-16 11:21:57 +0200

    Tests sans Bases de données fonctionels

--- Backend/app.js
+++ Backend/app.js
@@ -3,22 +3,32 @@ const express = require('express')
 const app = express();
 
 app.use((req, res, next) => {
-  console.log('Requête reçue !');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
 });
 
-app.use((req, res, next) => {
-  res.status(201);
-  next();
-});
-
-app.use((req, res, next) => {
-  res.json({ message: 'Votre requête a bien été reçue !' });
-  next();
-});
-
-app.use((req, res, next) => {
-  console.log('Réponse envoyée avec succès !');
-});
+app.use('/', (req, res, next) => {
+  const book = [
+    {
+      _id: 'foezhfouzeofu',
+      title: 'Mon 1er livre',
+      author: 'moi même',
+      year: '2',
+      genre: 'comédie',
+      averageRating: '3'
+    },
+    {
+      _id: 'foezhfouzeofu',
+      title: 'Mon 1er livre',
+      author: 'moi même',
+      year: '2',
+      genre: 'comédie',
+      averageRating: '3'
+    },
+  ];
+  res.status(200).json(book);
+})
 
 module.exports = app;
